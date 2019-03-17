f7cec43767012d614230a1620bdf47ca84693306
lndubose <lndubose13@gmail.com>
2019-03-17 15:08:08 -0500

    Finish callRep function, add message to recordMessage
    Have added my number to be the representative when I run it. Put my
    number in .env so it was not made public. So easily can add a representative
    number as an environment variable.

--- __init__.py
+++ __init__.py
@@ -1,23 +1,27 @@
 from flask import Flask, request, url_for
 from twilio.twiml.voice_response import Record, Redirect, VoiceResponse, Hangup
+from setttings import auth_token, account_sid, repNumber
 
 app = Flask(__name__)
+
+# Global Variables
 
 
 @app.route("/helper", methods=['GET', 'POST'])
 def welcome():
     resp = VoiceResponse()
 
-    resp.say("Thank you for calling Helper Bees. If this is a real medical" +
+    resp.say("Thank you for calling Helper Bees. If this is a real medical " +
              "emergency, please hang up and dial 911.")
 
     with resp.gather(num_digits=1, action=url_for("parse"), method='POST') \
             as gather:
-        gather.say("If you are calling about a concern with a client press 1" +
-                   ",unable to make a visit press 2," +
-                   "for time sheets press 3" +
-                   ",for scheduling press 4," +
-                   "for payments press 5," +
+        gather.say("If you are calling about a " +
+                   "concern with a client press 1, " +
+                   "unable to make a visit press 2, " +
+                   "for time sheets press 3, " +
+                   "for scheduling press 4, " +
+                   "for payments press 5, " +
                    "and all other options press 6.")
 
     resp.redirect(url_for('welcome'))
@@ -27,6 +31,8 @@ def welcome():
 @app.route("/parse", methods=['GET', 'POST'])
 def parse():
     resp = VoiceResponse()
+
+    print(request.values)
 
     if 'Digits' in request.values:
         choice = request.values['Digits']
@@ -50,12 +56,22 @@ def parse():
 
 
 def callRep(resp):
-    resp.say("Calling rep")
+    resp.say("Please hold as we connect you with " +
+             "a Customer Service Representative.")
+    resp.dial(repNumber)
     return resp
+
+# request value: CallSid = sid, ApiVersion = '2010-04-01'
+# From = number calling also Caller is the same
 
 
 def recordMessage(resp):
-    resp.say("Record message")
+    resp.say("Thank you for calling Helper Bees, " +
+             "if you will leave a message, " +
+             "we will get back to you as soon as possible. " +
+             "After the beep, please leave your first and last name, " +
+             "phone number, your client's name and your message. Thank you.")
+
     return resp
 
 
